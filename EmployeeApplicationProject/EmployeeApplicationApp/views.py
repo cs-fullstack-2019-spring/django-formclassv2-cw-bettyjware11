956f981550007cc56396c6c1cd972c0d35aa0fef
student <bettyjcawthon@gmail.com>
2019-02-27 16:56:33 -0600

    morning class

--- EmployeeApplicationProject/EmployeeApplicationApp/views.py
+++ EmployeeApplicationProject/EmployeeApplicationApp/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from .forms import EmployeeApplicationForm
 
-# Create your views here.
 
 # this function is used to test whether the server is working
 def index(request):
     return HttpResponse("EmployeeApplicationApp")
 
+# function puts info into new form
 def applicationForm(request):
     newForm = EmployeeApplicationForm()
 
@@ -23,10 +23,14 @@ def listEmployeeInfo(request):
     print(request.POST["name"])
     return HttpResponse("Listed Employee Info")
 
+# this function extracts information, saves and  puts it in a new form
 def application(request):
 
     if(request.method == 'POST'):
         print(request.POST)
+        newForm = EmployeeApplicationForm(request.POST)
+        if newForm.is_valid():
+            newForm.save(commit=True)
         context = {"name": request.POST["name"]}
         return render(request, "EmployeeApplicationApp/listEmployeeInfo.html", context)
     else:
